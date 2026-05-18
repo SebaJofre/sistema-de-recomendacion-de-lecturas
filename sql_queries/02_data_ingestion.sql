e2e82475ccad087f55113d24986b1f209c394e4d
Sebastián Jofré <135879706+SebaJofre@users.noreply.github.com>
2026-05-18 17:16:42 +0200

    Add files via upload

---
+++ sql_queries/02_data_ingestion.sql
@@ -0,0 +1,29 @@
+-- ==========================================================================================
+-- PASO 2: PIPELINE DE INGESTA DE DATOS
+-- DESCRIPTION: INGESTA MASIVA DEL DATASET DE LIBROS PREPROCESADOS EN FORMATO .csv.
+-- ==========================================================================================
+
+-- El archivo origen ha pasado por una fase previa de auditoría y limpieza en 
+-- Google Sheets para garantizar que los delimitadores y encodigs no rompan la ingesta.
+
+COPY db_books (
+    bookID, 
+    title, 
+    authors, 
+    avg_rating, 
+    isbn, 
+    isbn13, 
+    language_code, 
+    num_pages, 
+    rating_counts, 
+    text_review_counts, 
+    publication_date, 
+    publisher
+)
+FROM 'G:/My Drive/Data Analysis/Portfolio/Proyecto 1 - Books/Dataset/books_clean.csv'
+WITH (
+    FORMAT CSV, 
+    HEADER true, 
+    DELIMITER ',', 
+    ENCODING 'UTF8'
+);
